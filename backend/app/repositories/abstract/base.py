ea4da125002368386fba471cbf9471ef87e7b450
kamurano <105947149+striker06@users.noreply.github.com>
2024-09-30 11:53:44 +0400

    add baserepository

---
+++ backend/app/repositories/abstract/base.py
@@ -0,0 +1,88 @@
+from typing import Iterable
+from uuid import UUID
+
+from sqlmodel import select
+
+from app.core.database import Database
+from app.models.abstract.base import Base
+from app.repositories.interfaces.repository import (
+    IRepository,
+    M, C, U
+)
+
+
+class BaseRepository(IRepository[M, C, U]):
+    def __init__(
+        self,
+        database: Database,
+        model: M,
+    ) -> None:
+        self.produce_session = database.produce_session
+        self.model = model
+
+    async def create(self, schema: C) -> M:
+        async with self.produce_session() as session:
+            obj = self.model.model_validate(schema, from_attributes=True)
+            session.add(obj)
+            await session.commit()
+            await session.refresh(obj)
+            return obj
+
+    async def get(self, id: UUID) -> M:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)
+            result = await session.execute(stmt)
+            obj = result.scalars().unique().one_or_none()
+
+            return obj
+
+    async def update(self, id: UUID, schema: U) -> M:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)
+            result = await session.execute(stmt)
+            obj: Base = result.scalars().unique().one_or_none()
+
+            if not obj: return None
+
+            obj.model_update(schema)
+
+            await session.commit()
+            await session.refresh(obj)
+
+            return obj
+
+    async def delete(self, id: UUID) -> None:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)
+            result = await session.execute(stmt)
+            obj = result.scalars().unique().one_or_none()
+
+            await session.delete(obj)
+            await session.commit()
+
+    async def get_all(self) -> Iterable[M]:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model)
+            result = await session.execute(stmt)
+            objs = result.scalars().unique().all()
+
+            return objs
+
+    async def get_all_paginated(
+        self,
+        page: int,
+        page_size: int
+    ) -> Iterable[M]:
+        offset = (page - 1) * page_size
+
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).offset(offset).limit(page_size)
+            result = await session.execute(stmt)
+            obj = result.scalars().all()
+
+            return obj
---
+++ backend/app/repositories/interfaces/repository.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Iterable
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from app.models.abstract.base import Base
+
+
+M = TypeVar('M', bound=Base)
+C = TypeVar('C', bound=BaseModel)
+U = TypeVar('U', bound=BaseModel)
+
+
+class IRepository(ABC, Generic[M, C, U]):
+    @abstractmethod
+    async def create(self, model: C) -> M:
+        ...
+
+    @abstractmethod
+    async def get(self, id: UUID) -> M:
+        ...
+
+    @abstractmethod
+    async def update(self, id: UUID, model: U) -> M:
+        ...
+
+    @abstractmethod
+    async def delete(self, id: UUID) -> None:
+        ...
+
+    @abstractmethod
+    async def get_all(self) -> Iterable[M]:
+        ...
+
+    @abstractmethod
+    async def get_all_paginated(
+        self,
+        page: int,
+        page_size: int
+    ) -> Iterable[M]:
+        ...
