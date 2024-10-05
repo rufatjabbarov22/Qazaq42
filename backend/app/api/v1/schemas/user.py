d01da2ddc63f86a4950ba4fccd9b7ee748b679a2
samiribrh <samiribrahimov2277@gmail.com>
2024-10-05 20:34:15 +0400

    User Schema and Base model imporove

--- backend/app/api/v1/schemas/user.py
+++ backend/app/api/v1/schemas/user.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel, EmailStr, Field, field_validator
+import re
 from typing import Optional
 
-import re
+from fastapi import HTTPException, status
+from pydantic import BaseModel, EmailStr, field_validator, Field
 
 
 class UserCreate(BaseModel):
-    fname: str = Field(..., min_length=1, max_length=255)
-    lname: str = Field(..., min_length=1, max_length=255)
+    fname: str = Field(min_length=1, max_length=255)
+    lname: str = Field(min_length=1, max_length=255)
     email: EmailStr
-    password: str = Field(..., min_length=8, max_length=128)
-    profile_img: str = None
+    password: str = Field(min_length=8, max_length=128)
+    profile_img_path: Optional[str] = None
 
     @field_validator("password", mode="before")
     def validate_password(self, value: str) -> str:
@@ -21,7 +22,7 @@ class UserUpdate(BaseModel):
     lname: Optional[str] = Field(None, min_length=1, max_length=255)
     email: Optional[EmailStr] = None
     password: Optional[str] = Field(None, min_length=8, max_length=128)
-    profile_img: Optional[str] = None
+    profile_img_path: Optional[str] = None
     is_verified: Optional[bool] = None
     is_admin: Optional[bool] = None
 
@@ -34,11 +35,19 @@ class UserUpdate(BaseModel):
 
 def validate_password(value: str) -> str:
     if not re.search(r"[a-z]", value):
-        raise ValueError("Password must contain at least one lowercase letter")
+        raise HTTPException(
+            detail="Password must contain at least one lowercase letter",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
     if not re.search(r"[A-Z]", value):
-        raise ValueError("Password must contain at least one uppercase letter")
+        raise HTTPException(
+            detail="Password must contain at least one uppercase letter",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
     if not re.search(r"[0-9]", value):
-        raise ValueError("Password must contain at least one digit")
+        raise HTTPException(
+            detail="Password must contain at least one digit",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
     if not re.search(r"[!@#$%^&*]", value):
-        raise ValueError("Password must contain at least one special character (e.g., !@#$%^&*)")
+        raise HTTPException(
+            detail="Password must contain at least one special character (e.g., !@#$%^&*)",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
     return value
--- backend/app/models/abstract/base.py
+++ backend/app/models/abstract/base.py
@@ -2,7 +2,6 @@ from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel
-from sqlalchemy import func
 from sqlmodel import SQLModel, Field, TIMESTAMP
 
 
@@ -14,12 +13,12 @@ class Base(SQLModel):
         unique=True
     )
     created_at: datetime = Field(
-        sa_type=TIMESTAMP(timezone=True),
+        sa_type=TIMESTAMP(timezone=True),  # type: ignore
         nullable=False,
         default_factory=lambda: datetime.now(timezone.utc)
     )
     updated_at: datetime = Field(
-        sa_type=TIMESTAMP(timezone=True),
+        sa_type=TIMESTAMP(timezone=True),  # type: ignore
         nullable=False,
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column_kwargs={
