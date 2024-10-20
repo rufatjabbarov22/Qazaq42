5d59500f175a7299d4b2d4eadc80c0739541780f
samiribrh <samiribrahimov2277@gmail.com>
2024-10-20 11:20:50 +0400

    District Model

---
+++ backend/app/schemas/district.py
@@ -0,0 +1,34 @@
+from uuid import UUID
+
+from typing import Optional, List
+
+from pydantic import BaseModel, Field
+
+
+class DistrictCreate(BaseModel):
+    name: str
+    info: Optional[dict] = None
+
+    class Config:
+        orm_mode = True
+
+
+class DistrictUpdate(BaseModel):
+    name: Optional[str] = None
+    info: Optional[dict] = None
+
+    class Config:
+        orm_mode = True
+
+
+class DistrictRead(BaseModel):
+    id: UUID
+    country_id: str
+    name: str
+    info: dict
+    fields: Optional[List["FieldRead"]] = None  # type: ignore
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
+
+    class Config:
+        orm_mode = True
