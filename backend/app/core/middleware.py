eed624a91d96e2fd6db26239382f43d540e63d79
samiribrh <samiribrahimov2277@gmail.com>
2024-10-27 11:41:04 +0400

    Cleaner file structure

---
+++ backend/app/core/middleware.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.config import settings  # type: ignore
+
+
+def setup_middlewares(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,  # type: ignore
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
