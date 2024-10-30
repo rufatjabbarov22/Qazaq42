3e748b937ff50fe3824c8e0172e8288b7a8f20af
samiribrh <samiribrahimov2277@gmail.com>
2024-10-30 22:17:56 +0400

    Separate config and settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,32 +1,7 @@
-from typing import List
-
-from pydantic import DirectoryPath, FilePath, HttpUrl
+from pydantic import FilePath, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
-class Config(BaseSettings):
-    APP_TITLE: str
-    APP_VERSION: str
-    APP_DESCRIPTION: str
-
-    MEDIA_DIRECTORY_PATH: str
-    MEDIA_ALLOWED_EXTENSIONS: List[str]
-
-    LOG_DIRECTORY_PATH: DirectoryPath
-    LOG_LEVEL: str
-
-    ENVIRONMENT: str
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="allow"
-    )
-
-    def __init__(self):
-        super().__init__()
-
-
-config = Config()
+from config.config import Config, config
 
 
 class Secrets(BaseSettings):
