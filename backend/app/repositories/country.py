9f7a58f1ef5b081544341364fd507121b7b4709e
samiribrh <samiribrahimov2277@gmail.com>
2024-11-02 16:16:21 +0400

    Function overrides for Country Repository

--- backend/app/repositories/country.py
+++ backend/app/repositories/country.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, override
 from uuid import UUID
 
 from sqlalchemy.future import select
@@ -18,3 +18,46 @@ class CountryRepository(BaseRepository[Country, CountryCreate, CountryUpdate]):
             stmt = select(Country).join(District, Country.districts).where(District.id == district_id)  # type: ignore
             result = await session.execute(stmt)
             return result.scalar_one_or_none()
+
+    @override
+    async def get(self, id: str) -> Optional[Country]:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)  # type: ignore
+            result = await session.execute(stmt)
+            country = result.scalars().unique().one_or_none()
+
+            return country
+
+    @override
+    async def update(self, id: str, schema: Country) -> Optional[Country]:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)  # type: ignore
+            result = await session.execute(stmt)
+            country = result.scalars().unique().one_or_none()
+
+            if not country:
+                return None
+
+            country.model_update(schema)
+
+            await session.commit()
+            await session.refresh(country)
+            return country
+
+    @override
+    async def delete(self, id: str) -> bool:
+        async with self.produce_session() as session:
+
+            stmt = select(self.model).where(self.model.id == id)  # type: ignore
+            result = await session.execute(stmt)
+            country = result.scalars().unique().one_or_none()
+
+            if country is None:
+                return False
+
+            await session.delete(country)
+            await session.commit()
+
+            return True
