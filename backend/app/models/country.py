9d7e4dfbd71a0e5c81f3086ec2ed6a6c53d953eb
samiribrh <samiribrahimov2277@gmail.com>
2024-11-02 15:45:15 +0400

    Country name unique

--- backend/app/models/country.py
+++ backend/app/models/country.py
@@ -7,7 +7,7 @@ class Country(Base, table=True):
     __tablename__ = "countries"
 
     id: str = Field(max_length=2, primary_key=True)
-    name: str = Field(max_length=255, nullable=False)
+    name: str = Field(max_length=255, nullable=False, unique=True)
     info: dict = Field(sa_type=JSON, default={})
 
     districts: list["District"] = Relationship(back_populates="country")  # type: ignore
