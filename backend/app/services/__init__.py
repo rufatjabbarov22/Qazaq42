2c85ea0077c01190d14e7a3b9c7d60e762f81827
samiribrh <samiribrahimov2277@gmail.com>
2024-11-08 20:17:50 +0400

    Configure Service Dependency

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,7 @@
+from app.services.country import CountryService
+from app.services.crop_report import CropReportService
+from app.services.device import DeviceService
+from app.services.district import DistrictService
+from app.services.field import FieldModelService
+from app.services.telemetry import TelemetryService
+from app.services.user import UserService
