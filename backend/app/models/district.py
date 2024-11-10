2b8cd629b88e19a39e3e102c329315b54e4390d0
samiribrh <samiribrahimov2277@gmail.com>
2024-11-10 20:10:50 +0400

    Making District info optional

--- backend/app/models/district.py
+++ backend/app/models/district.py
@@ -1,3 +1,5 @@
+from typing import Dict, Optional
+
 from sqlmodel import Field, JSON, Relationship
 from sqlalchemy import UniqueConstraint
 
@@ -8,9 +10,9 @@ from app.models.field import FieldModel
 class District(Base, table=True):
     __tablename__ = "districts"
 
-    country_id: str = Field(foreign_key="countries.id", max_length=2, nullable=False, default="AZ")
+    country_id: str = Field(foreign_key="countries.id", max_length=255, nullable=False, default="AZ")
     name: str = Field(max_length=255, nullable=False)
-    info: dict = Field(sa_type=JSON, default={})
+    info: Optional[Dict] = Field(sa_type=JSON, default={}, nullable=True)
 
     fields: list[FieldModel] = Relationship(back_populates="district")
     country: "Country" = Relationship(back_populates="districts")  # type: ignore
