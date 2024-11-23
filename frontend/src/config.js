8b0319cccd790be1aa683a41e112efc50d52c420
simararustam <simararustam@gmail.com>
2024-11-24 02:00:05 +0400

    url problem

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const Base_Url = 'http://localhost:8000/api/v1/';
-// const Base_Url = 'https://api.qazaq.live/api/v1/';
-=======
+// const Base_Url = 'http://localhost:8000/api/v1/';
 const Base_Url = 'https://api.qazaq.live/api/v1/';
->>>>>>> 4420e890e0d265cf4dc7157fcf6c826ddf4bf49a
 export default Base_Url;
   
