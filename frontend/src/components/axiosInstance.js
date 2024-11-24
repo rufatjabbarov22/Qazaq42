f9c8e0f876c2f21287238e1196199d4d9ad144a6
samiribrh <samiribrahimov2277@gmail.com>
2024-11-24 10:40:00 +0400

    axiosInstance BaseUrl

--- frontend/src/components/axiosInstance.js
+++ frontend/src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://api.qazaq.live/api/v1/',
   headers: {
     'Content-Type': 'application/json',
   },
