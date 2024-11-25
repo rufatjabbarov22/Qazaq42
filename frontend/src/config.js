6299090ea9a8006cd1d3684a89aec54dbc0b2296
simararustam <simararustam@gmail.com>
2024-11-26 01:52:16 +0400

    url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const Base_Url = 'http://localhost:8000/api/v1/';
-// const Base_Url = 'https://api.qazaq.live/api/v1/';
+// const Base_Url = 'http://localhost:8000/api/v1/';
+const Base_Url = 'https://api.qazaq.live/api/v1/';
 export default Base_Url;
   
